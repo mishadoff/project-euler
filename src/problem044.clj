6a9b09904cdd1c406ffa0b86b69d4107db995a5f
Mykhailo Kozik <mishadoff@gmail.com>
2011-10-09 01:33:06 +0300

    Problem 44 solved

---
+++ src/problem044.clj
@@ -0,0 +1,12 @@
+(ns mishadoff
+  (:use [clojure.contrib.math :only (abs)]))
+(defn is-pentagonal? [n]
+  (let [t (/ (inc (Math/sqrt (inc (* 24 n)))) 6)]
+    (if (and (pos? n) (= t (quot t 1))) true false)))
+(defn pentagonal-number [n]
+  (* n (/ (- (* 3 n) 1) 2)))
+(def pentagonals (map pentagonal-number (iterate inc 1)))
+(filter #(not (nil? %))
+  (for [i (take 10000 pentagonals) j (take 10000 pentagonals)]
+    (if (and (is-pentagonal? (- i j)) (is-pentagonal? (+ i j)))
+      (abs (- i j)))))
