856952a20f400f0e5d86d1dbab24cd1c5509bc5d
Mykhailo Kozik <mishadoff@gmail.com>
2011-10-08 16:02:11 +0300

    Modified to real solution

--- src/problem041.clj
+++ src/problem041.clj
@@ -1,10 +1,11 @@
 (ns mishadoff
   (:use [clojure.contrib.lazy-seqs :only (primes)])
-  (:use [clojure.contrib.combinatorics :only (lex-permutations subsets)]))
+  (:use [clojure.contrib.combinatorics :only (lex-permutations)]))
 (defn is-prime? [prime]
   (if (= 1 prime) false
     (not-any? #(zero? (rem prime %)) (take-while #(<= (* % %) prime) primes))))
-(defn all-perm [sq]
-  (sort (reduce concat (map lex-permutations (subsets sq)))))
-(first (drop-while #(not (is-prime? (Integer/parseInt (reduce str %))))
-     (reverse (all-perm [1 2 3 4 5 6 7 8 9]))))
+(defn all-pandigital-perm []
+  (sort (reduce concat (map lex-permutations (take 8 (map #(range 1 %) (iterate inc 2)))))))
+(Integer/parseInt (reduce str 
+  (first (drop-while #(not (is-prime? (Integer/parseInt (reduce str %))))
+    (reverse (all-pandigital-perm))))))
