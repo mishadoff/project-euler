5869a824ea29e3d61957fcec5e79d59ff9bc2951
Mykhailo Kozik <mishadoff@gmail.com>
2011-10-09 02:35:30 +0300

    Problem 46 solved

---
+++ src/problem046.clj
@@ -0,0 +1,15 @@
+(ns mishadoff
+  (:use [clojure.contrib.lazy-seqs :only (primes)]))
+(defn is-prime? [prime]
+  (if (= 1 prime) false
+    (not-any? #(zero? (rem prime %)) (take-while #(<= (* % %) prime) primes))))
+(defn is-square? [n]
+  (let [sqr (Math/sqrt n)]
+    (= sqr (quot sqr 1))))
+(defn is-writable-as-sum? [n]
+  (loop [candidate-primes (take-while #(< % n) primes)]
+    (if (empty? candidate-primes) false
+      (if (is-square? (/ (- n (first candidate-primes)) 2)) true
+        (recur (rest candidate-primes))))))
+(def *composites* (filter #(and (odd? %) (not (is-prime? %))) (iterate inc 2)))
+(first (drop-while #(is-writable-as-sum? %) *composites*))
