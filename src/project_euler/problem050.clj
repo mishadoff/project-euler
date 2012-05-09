78f61f0b7f933570890eaf6d14ce91a457246724
mishadoff <mishadoff@gmail.com>
2012-05-09 20:38:16 +0300

    Problem 50 solved

--- src/project_euler/problem050.clj
+++ src/project_euler/problem050.clj
@@ -1,25 +1,21 @@
-(ns mishadoff
+(ns project-euler
   (:use [clojure.contrib.lazy-seqs :only (primes)]))
 
 (defn prime? [prime]
   (if (= 1 prime) false
     (not-any? #(zero? (rem prime %)) (take-while #(<= (* % %) prime) primes))))
 
-(defn make-seq-accumulator [seq]
-  (map first (iterate (fn [[sum s]]
-            [(+ sum (first s)) (next s)])
-              [(first seq) (rest seq)])))
- 
-(def prime-sums (conj (make-seq-accumulator primes) 0))
+(def prime-sums
+  (map first
+       (iterate (fn [[sum s]] [(+ sum (first s)) (rest s)])
+                [0 primes])))
 
 (defn euler-50 []
-  (loop [c   1]
-    (let [bots (reverse (take c prime-sums))
-      tops (take c (reverse (take-while #(> 1000000 (- % (last bots)))
-                        (rest prime-sums))))]
-      (if-let [v (some #(if (prime? %) % nil)
-               (map #(- %1 %2) tops bots))]
-    v
-    (recur (inc c))))))
- 
-(euler-50 1000000)
+  (loop [c 1]
+    (let [sums (reverse (take c prime-sums))
+          subs (take c (reverse (take-while #(> 1000000 (- % (last sums)))
+                                            (rest prime-sums))))]
+      (if-let [el (some #(if (prime? %) % nil)
+                        (map #(- %1 %2) subs sums))]
+        el
+        (recur (inc c))))))
