dcfd313b99877d8d72feac21a3bc29396ce024b9
mishadoff <mishadoff@gmail.com>
2012-12-11 19:41:53 +0200

    Problem 009 changed

--- src/project_euler/problem009.clj
+++ src/project_euler/problem009.clj
@@ -1,10 +1,10 @@
 (ns project-euler)
 
 (defn is-triplet? [a b c]
-  (and (> c b a) (= (+ (* a a) (* b b)) (* c c))))
+  (= (+ (* a a) (* b b)) (* c c)))
 
-;; Elapsed time: 240.380779 msec
+;; Elapsed time: 42.579438 msecs
 (defn euler-009 []
-  (first
-   (for [a (range 1 1000) b (range a 1000) c [(- 1000 a b)]
-         :when (is-triplet? a b c)] (* a b c))))
+  (first (for [a (range 1 1000) b (range a (- 1000 a))
+               :let [c (- 1000 a b)]
+               :when (and (> c b) (is-triplet? a b c))] (* a b c))))
