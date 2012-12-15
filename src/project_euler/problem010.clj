4fed906e7a21fca8db4d818bc1e27ad08fc9037f
mishadoff <mishadoff@gmail.com>
2012-12-15 19:40:44 +0200

    Problem 010 optimized

--- src/project_euler/problem010.clj
+++ src/project_euler/problem010.clj
@@ -4,3 +4,9 @@
 ;; Elapsed time: 12975.457287 msecs
 (defn euler-010 []
   (reduce + (take-while #(< % 2000000) primes)))
+
+;; small optimization to problem
+(defn sieve []
+  (loop [nums (set (cons 2 (range 3 2000000 2))) n 3]
+    (if (> (* n n) 2000000) (reduce + nums)
+        (recur (clojure.set/difference nums (range (* n n) 2000000 n)) (+ n 2)))))
