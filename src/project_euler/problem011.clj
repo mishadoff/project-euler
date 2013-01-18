a99b8e755487e80e25c7548bbdf3687f3d41ee1a
mishadoff <mishadoff@gmail.com>
2013-01-18 14:35:30 +0200

    Problem 011 refactored

--- src/project_euler/problem011.clj
+++ src/project_euler/problem011.clj
@@ -1,16 +1,18 @@
 (ns project-euler)
 
-(def matrix
-  (map #(Integer/parseInt %)(map #(reduce str %) (partition 2 2 (remove #(or (= \newline %) (= \ %))
-    (seq (slurp "res/problem011.txt")))))))
+(defn get-matrix []
+  (map #(Integer/parseInt (apply str %))
+       (partition 2 2 (remove #(or (= \newline %) (= \ %))
+                              (seq (slurp "res/problem011.txt"))))))
 
 (defn get-at [i j matrix]
   (if (and (>= i 0) (< i 20) (>= j 0) (< j 20))
-    (nth matrix (+ i (* j 20))) 0))
+    (nth matrix (+ j (* i 20))) 0))
 
-;; Elapsed time: 184.435329 msecs
+;; Elapsed time: 152.426437 msecs
 (defn euler-011 []
-  (let [ways (for [i (range 20) j (range 20)]
+  (let [matrix (get-matrix)
+        ways (for [i (range 20) j (range 20)]
                [(map #(get-at i (+ % j) matrix) (range 4))
                 (map #(get-at (+ % i) j matrix) (range 4))
                 (map #(get-at (+ % i) (+ % j) matrix) (range 4))
