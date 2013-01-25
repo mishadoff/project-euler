f42d27e2f2785a24a21028c2b35a047d198cbb7d
mishadoff <mishadoff@gmail.com>
2013-01-25 13:36:47 +0200

    Problem 012 rewritten

--- src/project_euler/problem012.clj
+++ src/project_euler/problem012.clj
@@ -1,4 +1,6 @@
-(ns project-euler)
+(ns project-euler
+  (:use [clojure.contrib.lazy-seqs :only (primes)])
+  (:use [clojure.contrib.math :only (sqrt)]))
 
 (defn triangle-number [n]
   (* n (/ (+ n 1) 2)))
@@ -6,8 +8,17 @@
 (def triangles (map triangle-number (iterate inc 1)))
 
 (defn num-of-divisors [n]
-  (+ 2 (* 2 (count (filter #(= (mod n %) 0) (range 2 (Math/sqrt n)))))))
+  (* 2 (count (filter #(zero? (mod n %)) (range 1 (sqrt n))))))
 
-;; Elapsed time: 12934.742749 msecs
+(defn factorize [n]
+  (loop [x n [p & ps] primes factors []]
+    (cond (= 1 x) factors
+          (zero? (mod x p)) (recur (/ x p) primes (conj factors p))
+          :else (recur x ps factors))))
+
+(defn factorize-count [n]
+  (reduce * (map (comp inc count) (vals (group-by identity (factorize n))))))
+
+;; Elapsed time: 2850.205718 msecs
 (defn euler-012 []
-  (first (drop-while #(< (num-of-divisors %) 500) triangles)))
+  (first (drop-while #(< (factorize-count %) 500) triangles)))
