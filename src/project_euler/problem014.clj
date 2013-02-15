379d84bccb03ac3c4aa268b26ff7da61012cdc25
mishadoff <mishadoff@gmail.com>
2013-02-16 00:55:58 +0200

    Problem 014 rewritten

--- src/project_euler/problem014.clj
+++ src/project_euler/problem014.clj
@@ -3,10 +3,12 @@
 (defn collatz-next [n]
   (if (even? n) (/ n 2) (inc (* n 3))))
 
-(defn collatz-chain-length [n]
-  [n (inc (count (take-while #(> % 1) (iterate collatz-next n))))])
+(defn collatz-chain-recursive [n]
+  (if (= n 1) 1
+      (inc (collatz-chain-recursive (collatz-next n)))))
 
-;; Elapsed time: 61465.886757
+;; Elapsed time: 12668.080554 msecs
 (defn euler-014 []
-  (first (reduce #(if (> (nth %1 1) (nth %2 1)) %1 %2)
-                 (map #(collatz-chain-length %) (range 1 1000000)))))
+  (first
+   (apply max-key second
+          (map #(list % (collatz-chain-recursive %)) (range 1 1000000)))))
