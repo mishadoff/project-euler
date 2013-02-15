c828e15a327879f8ec06ce890ec34aaf7d1bb3ec
mishadoff <mishadoff@gmail.com>
2013-02-15 22:51:38 +0200

    Problem 014 baseline time updated

--- src/project_euler/problem014.clj
+++ src/project_euler/problem014.clj
@@ -6,7 +6,7 @@
 (defn collatz-chain-length [n]
   [n (inc (count (take-while #(> % 1) (iterate collatz-next n))))])
 
-;; Elapsed time: 100103.273053 msecs
+;; Elapsed time: 61465.886757
 (defn euler-014 []
   (first (reduce #(if (> (nth %1 1) (nth %2 1)) %1 %2)
                  (map #(collatz-chain-length %) (range 1 1000000)))))
