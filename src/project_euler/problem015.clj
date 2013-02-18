63901f67daac6c5053bccb9670bd241a429807d3
mishadoff <mishadoff@gmail.com>
2013-02-19 00:29:27 +0200

    Problem 015 rewritten

--- src/project_euler/problem015.clj
+++ src/project_euler/problem015.clj
@@ -3,14 +3,10 @@
 (defn routes-extend [lst]
   (let [size (count lst)]
     (for [i (range (inc size))]
-      (if (or (= 0 i) (= size i)) 1 
+      (if (or (= 0 i) (= size i)) 1
         (+ (nth lst (dec i)) (nth lst i))))))
 
-(defn routes-narrow [lst]
-  (map + lst (rest lst)))
-
-;; Elapsed time: 15.202699 msecs
+;; Elapsed time: 14.809846 msecs
 (defn euler-015 []
-  (let [n 20]
-    (first (last (take (inc n) (iterate routes-narrow 
-                                        (last (take (inc n) (iterate routes-extend [1])))))))))
+  (let [n 20 d (inc (* n 2))]
+    (nth (last (take d (iterate routes-extend [1]))) n)))
