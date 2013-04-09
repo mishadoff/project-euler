796b23e752d8839e47f7aaceb928649059733da7
mishadoff <mishadoff@gmail.com>
2013-04-09 23:44:13 +0300

    Proble 017 rewritten

--- src/project_euler/problem017.clj
+++ src/project_euler/problem017.clj
@@ -1,17 +1,27 @@
 (ns project-euler)
 
-;; Elapsed time: 0.271962 msecs
+(def first20 ["one" "two" "three" "four" "five"
+              "six" "seven" "eight" "nine" "ten"
+              "eleven" "twelve" "thirteen" "fourteen" "fifteen"
+              "sixteen" "seventeen" "eighteen" "nineteen"])
+(def decas ["" "ten" "twenty" "thirty" "forty" "fifty" "sixty" "seventy" "eighty" "ninety"])
+(def h "hundred")
+(def t "thousand")
+(def a "and")
+
+(defn word-length [n]
+  (cond (= n 1000) (+ (count t) (count (nth first20 1)))
+        (< n 100)
+        (let [q (quot n 10) m (mod n 10)
+              d (count (nth decas q))]
+          (if (zero? m) d
+              (if (< q 2) (count (nth first20 (dec (+ m (* 10 q)))))
+                  (+ d (count (nth first20 (dec m)))))))
+        (< n 1000)
+        (let [q (quot n 100) m (mod n 100)]
+          (if (zero? m) (+ (word-length q) (count h))
+              (+ (count a) (count h) (word-length q) (word-length m))))))
+
+;; Elapsed time: 30.973979 msecs
 (defn euler-017 []
-  (let [digits ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine"]
-        numbers-after-ten ["ten" "eleven" "twelve" "thirteen" "fourteen" 
-                           "fifteen" "sixteen" "seventeen" "eighteen" "nineteen"]
-        numbers-10 ["twenty" "thirty" "forty" "fifty" "sixty" "seventy" "eighty" "ninety"]
-        hundred ["hundred"] thousand ["thousand"] and-symbol ["and"]]
-    (+ 
-     (* 190 (reduce + (map count digits)))
-     (* 10 (reduce + (map count numbers-after-ten)))
-     (* 100 (reduce + (map count numbers-10)))
-     (* 900 (reduce + (map count hundred)))
-     (* 891 (reduce + (map count and-symbol)))
-     (* 1 (count (nth digits 0)))
-     (* 1 (reduce + (map count thousand))))))
+  (reduce + (map word-length (range 1 1001))))
