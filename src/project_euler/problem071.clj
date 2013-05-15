f91c7846ec1cf2e44f3c4e6913b1407a87acb9b0
mishadoff <mishadoff@gmail.com>
2013-05-15 15:24:51 +0300

    Problem 071 added.

--- src/project_euler/problem071.clj
+++ src/project_euler/problem071.clj
@@ -1,5 +1,20 @@
 (ns project-euler)
 
+;; as bruteforce solution on farey sequence is very slow
+;; we use property of 'Farey Pair' http://en.wikipedia.org/wiki/Farey_sequence#Farey_neighbours
+
+;; a/b < c/d and they are neigbours
+;; their difference 1/bd
+;; bc - ad = 1
+;; c/d is 3/7
+;; 3b - 7a = 1
+;; It's a diofantine equation
+;;
+;; a = (3b - 1) / 7
+;; Solve it.
+
 (defn euler-071 []
-  ;; TODO
-  )
+  (loop [b 1000000]
+    (let [tmp (dec (* 3 b))]
+      (if (zero? (rem tmp 7)) [(/ tmp 7) b]
+          (recur (dec b))))))
