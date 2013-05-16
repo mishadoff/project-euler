194031b4fdaf39c9d20176910c53ce7c52d7e04a
mishadoff <mishadoff@gmail.com>
2013-05-16 18:05:57 +0300

    Problem 066 solved

--- src/project_euler/problem066.clj
+++ src/project_euler/problem066.clj
@@ -1,22 +1,45 @@
 (ns project-euler)
 
+;; This Diophantine equation known as Pell's equation
+;; http://en.wikipedia.org/wiki/Pell%27s_equation
+;;
+;; Let h_i/k_i denote the sequence of convergents to the continued fraction sqrt(n)
+;; Then the pair (x,y) solving Pell's equation and minimizing x,
+;; x = h_i, y = k_i. Fundamental solution.
+
 (defn square? [n]
-  (let [r (int (Math/sqrt n))]
-    (= (* r r) n)))
+  (let [a (int (Math/sqrt n))]
+    (= n (* a a))))
 
-(defn diophantine-solution [d]
-  (if-not (square? d)
-    (loop [x 1 y 1]
-      (print x y)
-      (let [left (dec (* x x))
-            right (* d y y)]
-        (cond (> right left)
-              (recur (inc x) (int (Math/sqrt (/ left d))))
-              (< right left) (recur x (inc y))
-              (= right left) [x y])))))
+(defn convergent [seq]
+  "Number of convergents, sequence"
+  (letfn [(partial-sum [fseq]
+            (reduce #(+ (/ 1 %1) %2) (reverse fseq)))] ;; TODO improve performance
+    (map #(partial-sum (take % seq)) (iterate inc 1))))
 
+(defn continued-fraction-sqroot-seq [n]
+  "TODO document"
+  (let [a0 (int (Math/sqrt n))]
+    (letfn [(next-frac [a m d]
+              (let [m1 (- (* a d) m)
+                    d1 (/ (- n (* m1 m1)) d)
+                    a1 (quot (+ a0 m1) d1)]
+                (cons a1 (lazy-seq (next-frac a1 m1 d1)))))]
+      (if (= (* a0 a0) n) (list a0)
+          (cons a0 (lazy-seq (next-frac a0 0 1)))))))
+
+(defn solution? [x y d]
+  (zero? (- (* x x)
+            (* d y y) 1)))
+
+(defn solve-fundamental [d]
+  (numerator
+   (first (filter #(solution? (numerator %) (denominator %) d)
+                  (filter ratio?
+                          (convergent (continued-fraction-sqroot-seq d)))))))
+
+;; Elapsed time: 504.251763 msecs
 (defn euler-066 []
-  (first
-   (sort-by #(max (second %1) (second %2))
-            (for [d (range 1 100)]
-              [d (first (diophantine-solution d))]))))
+  (apply max-key first
+         (map #(list (solve-fundamental %) %)
+              (filter (comp not square?) (range 1 1001)))))
