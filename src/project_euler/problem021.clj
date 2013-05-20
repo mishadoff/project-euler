3513c830014bbf0519b31a7d2f28be0b6c57fb29
mishadoff <mishadoff@gmail.com>
2013-05-21 02:10:07 +0300

    Problem 021 improved performance x10

--- src/project_euler/problem021.clj
+++ src/project_euler/problem021.clj
@@ -1,17 +1,17 @@
 (ns project-euler)
 
 (defn sum-of-proper-divisors [n]
-  (let [base (filter #(= (mod n %) 0) (range 2 (Math/sqrt n)))]
-    (inc (reduce + (concat (map #(/ n %) base) base)))))
+  (let [base (filter #(zero? (mod n %)) (range 2 (Math/sqrt n)))]
+    (reduce + 1 (concat (map #(/ n %) base) base))))
 
 (defn is-amicable? [a b]
-  (if (and (not (= a b))
-           (= a (sum-of-proper-divisors b))
-           (= b (sum-of-proper-divisors a))) true false))
+  (and (not (= a b))
+       (= a (sum-of-proper-divisors b))
+       (= b (sum-of-proper-divisors a))))
 
-;; Elapsed time: 2443.771657 msecs
+;; Elapsed time: 297.164885 msecs
 (defn euler-021 []
-  (reduce + (filter #(not (nil? %))
-                    (let [sums (map sum-of-proper-divisors (range 1 10000))]
-                      (for [i (range 0 (dec 10000))]
-                        (if (is-amicable? (inc i) (nth sums i)) (inc i)))))))
+  (reduce +
+          (let [sums (vec (map sum-of-proper-divisors (range 1 10000)))] ;; vec is important
+            (for [i (range 1 10000)]
+              (if (is-amicable? i (nth sums (dec i))) i 0)))))
