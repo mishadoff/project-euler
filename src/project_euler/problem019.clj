fafa336867107920528286c22de7485775f22160
mishadoff <mishadoff@gmail.com>
2013-07-10 02:21:34 +0300

    Problem 019 updated

--- src/project_euler/problem019.clj
+++ src/project_euler/problem019.clj
@@ -1,13 +1,16 @@
 (ns project-euler
   (import [java.util GregorianCalendar]))
 
-;; Elapsed time: 13.592651 msecs
+(defn calendar-for [year month]
+  (doto (GregorianCalendar.)
+    (.set GregorianCalendar/YEAR year)
+    (.set GregorianCalendar/MONTH month)
+    (.set GregorianCalendar/DAY_OF_MONTH 1)))
+
+;; Elapsed time: 30.138531 msecs
 (defn euler-019 []
   (reduce +
           (for [year (range 1901 (inc 2000)) month (range 1 (inc 12))]
-            (let [c (doto (GregorianCalendar.)
-                      (.set GregorianCalendar/YEAR year)
-                      (.set GregorianCalendar/MONTH month)
-                      (.set GregorianCalendar/DAY_OF_MONTH 1))]
+            (let [c (calendar-for year month)]
               (if (= GregorianCalendar/SUNDAY 
                      (.get c GregorianCalendar/DAY_OF_WEEK)) 1 0)))))
