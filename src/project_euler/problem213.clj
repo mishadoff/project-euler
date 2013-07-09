b19d81b8ef9738bdaabaceef71d9e2d12a0038f6
mishadoff <mishadoff@gmail.com>
2013-07-09 19:12:37 +0300

    Problem 213 progress

--- src/project_euler/problem213.clj
+++ src/project_euler/problem213.clj
@@ -1,6 +1,6 @@
 (ns project-euler)
 
-(def GRID_SIZE 3)
+(def GRID_SIZE 30)
 
 (defn init-field []
   (vec (map vec (repeat GRID_SIZE (repeat GRID_SIZE 1)))))
@@ -20,12 +20,23 @@
         rand-to (rand-nth (remove wrong-cell? cells))]
     (move field from rand-to)))
 
+(defn who-moves [field]
+  (apply concat
+          (for [i (range GRID_SIZE) j (range GRID_SIZE)]
+            (repeat (get-in field [i j]) [i j]))))
+
 (defn bell [field]
-  (let [cells (for [x (range GRID_SIZE) y (range GRID_SIZE)] [x y])]
+  (let [cells (who-moves field)]
     (reduce random-move field cells)))
+
+(defn experiment []
+  (unoccupied-count (first (drop 50 (iterate bell (init-field))))))
 
 (defn unoccupied-count [field]
   (count (filter zero? (flatten field))))
 
-(defn euler-213 []
-  )
+(defn euler-213 [n]
+  (double (/ (reduce +' 
+                     (for [i (range n)]
+                       (experiment)))
+             n)))
