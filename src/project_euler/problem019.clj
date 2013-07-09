13094cce6d617690d958a0d75456faab14c8aaac
mishadoff <mishadoff@gmail.com>
2013-07-10 01:57:43 +0300

    Problem 019 readability improved

--- src/project_euler/problem019.clj
+++ src/project_euler/problem019.clj
@@ -1,13 +1,13 @@
 (ns project-euler
   (import [java.util GregorianCalendar]))
 
-;; Elapsed time: 45.007752 msecs
+;; Elapsed time: 13.592651 msecs
 (defn euler-019 []
   (reduce +
-   (let [c (new GregorianCalendar)]
-     (for [year (range 1901 (inc 2000)) month (range 1 (inc 12))]
-       (do
-         (.set c GregorianCalendar/YEAR year)
-         (.set c GregorianCalendar/MONTH month)
-         (.set c GregorianCalendar/DAY_OF_MONTH 1)
-         (if (= GregorianCalendar/SUNDAY (.get c GregorianCalendar/DAY_OF_WEEK)) 1 0))))))
+          (for [year (range 1901 (inc 2000)) month (range 1 (inc 12))]
+            (let [c (doto (GregorianCalendar.)
+                      (.set GregorianCalendar/YEAR year)
+                      (.set GregorianCalendar/MONTH month)
+                      (.set GregorianCalendar/DAY_OF_MONTH 1))]
+              (if (= GregorianCalendar/SUNDAY 
+                     (.get c GregorianCalendar/DAY_OF_WEEK)) 1 0)))))
