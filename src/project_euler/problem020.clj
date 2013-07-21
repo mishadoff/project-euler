067868a548cf8b2e0484cffa6fe20491f1da95b2
mishadoff <mishadoff@gmail.com>
2013-07-21 13:50:23 +0300

    Problem 020 modified

--- src/project_euler/problem020.clj
+++ src/project_euler/problem020.clj
@@ -3,6 +3,9 @@
 (defn sum-of-digits [n]
   (reduce + (map #(- (int %) 48) (seq (str n)))))
 
+(defn ! [n]
+  (reduce *' (range 1 (inc n))))
+
 ;; Elapsed time: 1.74764 msecs
 (defn euler-020 []
-  (sum-of-digits (reduce *' (take 100 (iterate inc 1)))))
+  (sum-of-digits (! 100)))
