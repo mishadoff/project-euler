05a1061bd2910b4452dd96a37baf34a9bb16d42e
mishadoff <mishadoff@gmail.com>
2014-05-08 22:45:11 +0300

    Problem 23 algorithm changed

--- src/project_euler/problem023.clj
+++ src/project_euler/problem023.clj
@@ -1,18 +1,18 @@
 (ns project-euler)
 
 (defn sum-of-proper-divisors [n]
-  (let [base (filter #(zero? (mod n %)) (range 2 (inc (Math/sqrt n))))]
-    (inc (reduce + (set (concat base (map #(/ n %) base)))))))
+  (let [divs (filter #(zero? (mod n %)) (range 2 (Math/sqrt n)))]
+    (reduce + (concat divs (map #(/ n %) divs)))))
 
 (defn is-abundant? [n]
   (> (sum-of-proper-divisors n) n))
 
+(defn abundant-sum? [n abundant]
+  (some #(abundant (- n %))
+        (take-while #(< % n) abundant)))
+   
 ;; Elapsed time: 13783.380167 msecs
 (defn euler-023 []
-  (- (reduce + (range 1 28124))
-     (reduce + (set 
-                (let [abundant (filter is-abundant? (range 12 28124))]
-                  (for [i abundant j abundant :while (< (+ i j) 28124)]
-                    (+ i j)))))))
-      
-  
+  (let [abundant (apply sorted-set (filter is-abundant? (range 12 28124)))]
+    (reduce + (remove #(abundant-sum? % abundant)
+                      (range 12 28124)))))
