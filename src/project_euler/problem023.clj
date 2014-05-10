035f342d85cb14650fc60ca8cb100beea0263300
mishadoff <mishadoff@gmail.com>
2014-05-10 18:30:50 +0300

    Problem 023 cleanup

--- src/project_euler/problem023.clj
+++ src/project_euler/problem023.clj
@@ -1,22 +1,23 @@
 (ns project-euler)
 
 (defn sum-of-proper-divisors [n]
-  (let [divs (filter #(zero? (mod n %))
-                     (range 2 (Math/sqrt n)))]
-    (reduce + (set (concat [1]
-                           (let [isq (int (Math/sqrt n))]
-                             (if (= n (* isq isq)) [isq] []))
-                           divs
-                           (map #(/ n %) divs))))))
+  (let [divs (filter #(zero? (mod n %)) (range 2 (Math/sqrt n)))]
+    (reduce + 1 (set (concat
+                      (let [isq (int (Math/sqrt n))]
+                        (if (= n (* isq isq)) [isq] []))
+                      divs
+                      (map #(/ n %) divs))))))
 
-(defn is-abundant? [n]
+(defn abundant? [n]
   (> (sum-of-proper-divisors n) n))
 
 (defn abundant-sum? [n abundant]
   (some #(abundant (- n %))
         (take-while #(< % n) abundant)))
-   
+
 ;; Elapsed time: 14591.108689 msecs
 (defn euler-023 []
-  (let [abundant (into (sorted-set) (filter is-abundant? (range 12 28124)))]
-    (reduce + (remove #(abundant-sum? % abundant) (range 1 28124)))))
+  (let [abundant (into (sorted-set) (filter abundant? (range 12 28124)))]
+    (->> (range 1 28124)
+         (remove #(abundant-sum? % abundant))
+         (reduce +))))
