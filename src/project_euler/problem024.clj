6b6bc6c218bc59dc1973afb0310c80c0ca7c9521
mishadoff <mishadoff@gmail.com>
2014-08-21 16:52:30 +0300

    Clojure Problem 024: Clever algorithm implemented

--- src/project_euler/problem024.clj
+++ src/project_euler/problem024.clj
@@ -8,3 +8,28 @@
        (drop (dec 1000000))
        (first)
        (apply str)))
+
+(defn euler-024-clever []
+  (let [! (fn [n] (reduce * (range 1 (inc n))))]
+    (loop [available-digits (range 10)
+           num 1000000
+           current-digit 0
+           init 9
+           result []]
+      (let [f (! init)]
+        (cond
+         (= 0 init)
+         (apply str (concat result available-digits))
+         
+         (< f num) (recur available-digits
+                          (- num f)
+                          (inc current-digit)
+                          init
+                          result)
+
+         :else (recur (concat (take current-digit available-digits)
+                              (drop (inc current-digit) available-digits))
+                      num
+                      0
+                      (dec init)
+                      (conj result (nth available-digits current-digit))))))))
